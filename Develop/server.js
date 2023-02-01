653158e860a636d6973456622963a8e10a85250a
Avery <averycaldwell7@gmail.com>
2023-02-01 11:35:31 -0800

    edit comments

---
+++ Develop/server.js
@@ -0,0 +1,99 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+// Helper method for generating unique ids
+const uuid = require('uuid');
+// Initialize express app
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Setup data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+
+
+// On the back end, the application should include a db.json file that will be used to store and retrieve notes using the fs module.
+app.get('/api/notes', (req, res) => {
+    console.info('GET /api/notes');
+    let notes = JSON.parse(fs.readFileSync('./db/db.json'));
+    res.json(notes);
+});
+
+// Display notes.html when /notes is accessed
+app.get('/notes', (req, res) =>
+res.sendFile(path.join(__dirname, './public/notes.html'))
+);
+
+// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. 
+// Setup the /api/notes post route
+app.post('/api/notes', function (req, res) {
+    console.info('POST /api/notes');
+    // Receives a new note, adds it to db.json, then returns the new note
+    let body = req.body;
+    body.id = uuid.v4();
+    readAndAppend(body, './db/db.json');
+    res.send('Successfully Wrote Item ' + body.id);
+
+});
+// DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. In order to delete a note,
+// you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the
+// notes to the db.json file.
+app.delete('/api/notes/:id', function (req, res) {
+    console.info('DELETE /api/notes');
+    const id = req.params.id;
+    readAndDeleteById(id, './db/db.json');
+    res.send('Successfully Deleted ' + id);
+});
+// Function to read data from a given a file and append some content
+const readAndAppend = (content, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const parsedData = JSON.parse(data);
+            parsedData.push(content);
+            let output = JSON.stringify(parsedData);
+            fs.writeFile(file, output, (err) => {
+                if (err) {
+                    console.error(err);
+                }
+            });
+        }
+    });
+};
+
+const readAndDeleteById = (id, file) => {
+    let success;
+    fs.readFile(file, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            let index;
+            const parsedData = JSON.parse(data);
+            for (let i = 0; i < parsedData.length; i++) {
+                if (parsedData[i].id === id) {
+                    index = i;
+                    break;
+                }
+            }
+            parsedData.splice(index, 1); // 2nd parameter means remove one item only
+
+            let output = JSON.stringify(parsedData);
+            fs.writeFile(file, output, (err) => {
+                if (err) {
+                    console.error(err);
+                }
+            });
+        }
+    });
+    return success;
+};
+
+// Setup listener
+app.listen(PORT, function () {
+    console.log('App listening on PORT: ' + PORT);
+});
+
